acad6271b90a06c103f14224fb31d6ce188be839
badhitman <ru.usa.goo@gmail.com>
2022-07-13 20:11:56 +0300

    Update GeneratorCSharpService.cs

--- SharedLib/Services/codegenerator/csharp/GeneratorCSharpService.cs
+++ SharedLib/Services/codegenerator/csharp/GeneratorCSharpService.cs
@@ -692,7 +692,7 @@ namespace SharedLib.Services
                 service_type_name = service_type_name[1..];
                 enumEntry = archive.CreateEntry(Path.Combine(dir, "service_implementations", $"{service_type_name}.cs"));
                 writer = new(enumEntry.Open(), Encoding.UTF8);
-                await WriteHead(writer, project_info.Name, project_info.NameSpace, null);
+                await WriteHead(writer, project_info.Name, project_info.NameSpace, null, new string[] { "SharedLib.Models" });
 
                 await writer.WriteLineAsync($"\tpublic partial class {service_type_name} : I{service_type_name}");
                 await writer.WriteLineAsync("\t{");
