e13f0a205c7f9447946496c33ad3019c2068cae2
badhitman <ru.usa.goo@gmail.com>
2022-07-13 23:14:30 +0300

    patch for constructor

---
+++ SharedLib/Models/enums/RouteMethodsPrefixesEnum.cs
@@ -0,0 +1,62 @@
+﻿////////////////////////////////////////////////
+// © https://github.com/badhitman - @fakegov 
+////////////////////////////////////////////////
+
+namespace SharedLib.Models.enums
+{
+    /// <summary>
+    /// Префиксы маршрутов контроллеров (+ Refit)
+    /// </summary>
+    public enum RouteMethodsPrefixesEnum
+    {
+        /// <summary>
+        /// Добавить/создать один объект
+        /// </summary>
+        AddSingle,
+
+        /// <summary>
+        /// Добавить/создать коллекцию объектов
+        /// </summary>
+        AddRange,
+
+        /// <summary>
+        /// Получить один объект по идентификатору
+        /// </summary>
+        GetSingleById,
+
+        /// <summary>
+        /// Получить коллекцию элементов по идентификаторам
+        /// </summary>
+        GetRangeByIds,
+
+        /// <summary>
+        /// Получить коллекцию элементов по идентификатору ведущего объекта-владельца
+        /// </summary>
+        GetRangeByOwnerId,
+
+        /// <summary>
+        /// Обновить один объект
+        /// </summary>
+        UpdateSingle,
+
+        /// <summary>
+        /// Обновить коллекцию объектов
+        /// </summary>
+        UpdateRange,
+
+        /// <summary>
+        /// Пометить объект по идентификатору
+        /// </summary>
+        MarkAsDeleteById,
+
+        /// <summary>
+        /// Удалить (безвозвартно) объект по идентификатору
+        /// </summary>
+        RemoveSingleById,
+
+        /// <summary>
+        /// Удалить (безвозвартно) колекцию объектов по идентификаторам
+        /// </summary>
+        RemoveRangeByIds
+    }
+}
