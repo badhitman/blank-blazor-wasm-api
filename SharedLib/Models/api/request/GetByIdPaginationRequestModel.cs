792adf1f5972380d23d53fe6aac3ecf0bb1edb1d
badhitman <ru.usa.goo@gmail.com>
2022-07-19 19:34:06 +0300

    Create GetByIdPaginationRequestModel.cs

---
+++ SharedLib/Models/api/request/GetByIdPaginationRequestModel.cs
@@ -0,0 +1,17 @@
+﻿////////////////////////////////////////////////
+// © https://github.com/badhitman - @fakegov 
+////////////////////////////////////////////////
+
+namespace SharedLib.Models
+{
+    /// <summary>
+    /// Запрос по идентификатору (с пагинацией)
+    /// </summary>
+    public class GetByIdPaginationRequestModel : PaginationRequestModel
+    {
+        /// <summary>
+        /// Фильтр
+        /// </summary>
+        public int FilterId { get; set; }
+    }
+}
